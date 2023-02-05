fe10d069915544ef4d0239d8c9d45509fef96a64
Tanny <ryosuke.tanimura+dev@gmail.com>
2023-02-05 21:49:29 +0900

    Add: タブを追加、例外処理を追加

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 from datetime import date, datetime
 
-import numpy as np
 import pandas as pd
 import pandas_datareader.stooq as stooq
 import plotly.graph_objects as go
@@ -9,10 +8,15 @@ import streamlit as st
 
 @st.cache
 def download_stock_price(ticker: str, start: date, end: date):
-    return stooq.StooqDailyReader(ticker, start, end).read()
+    df_stock = stooq.StooqDailyReader(ticker, start, end).read()
+    if len(df_stock) == 0:
+        raise ValueError("No data found")
+    return df_stock
 
 
 st.title("Stock Price Dashboard")
+
+df_stock = pd.DataFrame()
 
 with st.sidebar.form("input_ticker"):
     ticker = st.text_input("Ticker", "AAPL")
@@ -20,32 +24,44 @@ with st.sidebar.form("input_ticker"):
     if submitted:
         end = datetime.now().date()
         start = datetime(end.year - 1, end.month, end.day).date()
-        df_stock = download_stock_price(ticker, start, end)
+        try:
+            df_stock = download_stock_price(ticker, start, end)
+        except ValueError as e:
+            st.error(e, icon="🚨")
 
-st.header("Dataframe")
-st.dataframe(df_stock.style.highlight_max(axis=0), use_container_width=True)
 
+tab1, tab2 = st.tabs(["🗒️ Table", "📈 Chart"])
 
-# https://myfrankblog.com/stock_price_chart_with_python_plotly/
+with tab1:
+    st.header("Dataframe")
+    if df_stock.empty:
+        st.info("Input the ticker and submit.", icon="ℹ️")
+    else:
+        st.dataframe(df_stock.style.highlight_max(axis=0), use_container_width=True)
 
-st.header("Candlestick chart")
-fig = go.Figure(
-    data=[
-        go.Candlestick(
-            x=df_stock.index,
-            open=df_stock["Open"],
-            high=df_stock["High"],
-            low=df_stock["Low"],
-            close=df_stock["Close"],
-            showlegend=False,
+with tab2:
+    # https://myfrankblog.com/stock_price_chart_with_python_plotly/
+    st.header("Candlestick chart")
+    if df_stock.empty:
+        st.info("Input the ticker and submit.", icon="ℹ️")
+    else:
+        fig = go.Figure(
+            data=[
+                go.Candlestick(
+                    x=df_stock.index,
+                    open=df_stock["Open"],
+                    high=df_stock["High"],
+                    low=df_stock["Low"],
+                    close=df_stock["Close"],
+                    showlegend=False,
+                )
+            ]
         )
-    ]
-)
-fig.update_layout(
-    title_text=f"CandlestickChart of {ticker}",
-    yaxis_title="Price",
-    xaxis_title="Date",
-)
-fig.update_xaxes(tickformat="%Y/%m/%d")
-fig.update_yaxes(separatethousands=True)
-st.plotly_chart(fig)
+        fig.update_layout(
+            title_text=f"CandlestickChart of {ticker}",
+            yaxis_title="Price",
+            xaxis_title="Date",
+        )
+        fig.update_xaxes(tickformat="%Y/%m/%d")
+        fig.update_yaxes(separatethousands=True)
+        st.plotly_chart(fig)
